ef81cc3df71413b0378feed35dff57aa36735e65
Marcel Hecko <marcel@sitpub.com>
2013-03-22 15:27:34 +0000

    [html] changed from puppet to html

---
+++ lib/puppet/reports/html.rb
@@ -0,0 +1,17 @@
+require 'puppet'
+require 'yaml'
+
+Puppet::Reports.register_report(:html) do
+
+  desc <<-DESC
+  Send report information to HTML file.
+  DESC
+
+  def process
+    $path = "/var/www/virtual/puppet/#{self.host}.html"
+    Puppet.debug "Sending status for #{self.host} to HTML file #$path"
+    file = File.open("#$path","w")
+    file.write("Puppet run for #{self.host} #{self.status}")
+    file.close
+  end
+end
